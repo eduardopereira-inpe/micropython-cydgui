e91c2b8057fb265606f6b1fbc18944ac384b1ccc
Eduardo dos Santos Pereira <eduardo.pereira@inpe.br>
2026-06-21 12:50:59 -0300

    feat: adicionar controle de tempo para o estado do backlight na classe BlState

--- cydgui/bl_state.py
+++ cydgui/bl_state.py
@@ -1,13 +1,22 @@
 from machine import Pin
 import uasyncio as asyncio
-
+import time
 class BlState:
-    __slots__ = ("pin", "pin_btn", "state", "verbose")
-    def __init__(self, pin_num, pin_btn, verbose=True):
+    __slots__ = (
+        "pin", 
+        "pin_btn", 
+        "state", 
+        "verbose", 
+        "dtime_minutes",
+        "_last_toggle_time"
+        )
+    def __init__(self, pin_num, pin_btn, verbose=True, dtime_minutes=5):
         self.pin = Pin(pin_num, Pin.OUT)
         self.pin_btn = Pin(pin_btn, Pin.IN, Pin.PULL_UP)
         self.state = False
+        self._last_toggle_time = 0
         self.verbose = verbose
+        self.dtime_minutes = dtime_minutes
 
     def _log(self, message):
         if self.verbose:
@@ -18,7 +27,14 @@ class BlState:
             self._log("Button pressed, toggling backlight state.")
             self.state = not self.state
             self.pin.value(self.state)
-        
+            self._last_toggle_time = time.ticks_ms()
+
+        if self.state and time.ticks_diff(time.ticks_ms(), self._last_toggle_time) > self.dtime_minutes * 60 * 1000:
+            self._log("Auto-toggling backlight off after timeout.")
+            self.state = False
+            self.pin.value(self.state)
+            self._last_toggle_time = time.ticks_ms()
+            
     async def monitor(self):
         while True:
             self.toggle()
